76e0abf6a204de97b05efbd6264918ba54b95b04
dianwib <2nd.wibowo@gmail.com>
2019-10-06 19:28:16 +0700

    first commit

--- extract_to_matrixS.py
+++ extract_to_matrixS.py
@@ -4,12 +4,11 @@ data=pd.read_csv("matrix_rating_3.csv")
 del data['Unnamed: 0']
 del data['User']
 data_2=data
-
-print(data.columns)
-
 for user in data.index:
+    print(user)
     temp_nilai_total_user=data.sum(axis=1,skipna=True)
     for item in data.columns:
-        data_2.at[user,item]=data.at[user,item]/temp_nilai_total_user
+        nilai_matrix_lama=float(data.at[user,item])
+        data_2.at[user,item]=nilai_matrix_lama/temp_nilai_total_user[user]
 
 data_2.to_csv("matrix_s.csv")
